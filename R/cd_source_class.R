e9694448952abebc70d456ab642f6e13c6f45d1c
Gavin Rhys Lloyd <grlloyd@users.noreply.github.com>
2022-11-23 14:41:08 +0000

    accommodate missing columns in CD3.1

--- R/cd_source_class.R
+++ R/cd_source_class.R
@@ -2,7 +2,7 @@
 #' @include import_annotation_class.R
 #' @export
 cd_source = function(
-    input_file, 
+        input_file, 
     compounds_file,
     tag = 'CD',
     cd_version = 3.1,
@@ -51,194 +51,201 @@ cd_source = function(
 
 #' @export
 setMethod(f = "model_apply",
-          signature = c("cd_source",'lcms_table'),
-          definition = function(M,D) {
-
-              CD_data <- openxlsx::readWorkbook(xlsxFile=M$input_file, sheet=2)
-              CD_isomers <- openxlsx::readWorkbook(xlsxFile=M$compounds_file, sheet=2)
-              
-              if (M$cd_version==3.3) {
-                  # replace all 'Tags' with NA
-                  CD_data[CD_data=='Tags']=NA
-                  CD_isomers[CD_isomers=='Tags']=NA
-                  # remove Tags column
-                  CD_data$Tags = NULL
-                  CD_isomers$Tags = NULL
-              }
-              unique_cd_compounds_ids <-which(!is.na(CD_data$Checked))
-              cd_compounds <- CD_data$Name[unique_cd_compounds_ids]
-              cd_formula <- gsub(" ", "", CD_data$Formula[unique_cd_compounds_ids])
-              cd_RT <- as.numeric(CD_data$`RT.[min]`[unique_cd_compounds_ids]) * 60
-              cd_mzcloud_score <- as.numeric(CD_data$mzCloud.Best.Match[unique_cd_compounds_ids])
-              cd_mz <- as.numeric(CD_data$`m/z`[unique_cd_compounds_ids])
-              
-              if (length(unique_cd_compounds_ids)>1){
-                  unique_cd_compounds_ids_end <- c((unique_cd_compounds_ids-1)[2:length(unique_cd_compounds_ids)], nrow(CD_data))
-              } else {
-                  unique_cd_compounds_ids_end <- nrow(CD_data)
-              }
-              
-              # CD output is stored in weird tree structure, compounds -> compounds_per_files -> features
-              # this loop will create summary feature table
-              cd_compounds_table <- vector("list", length(cd_compounds))
-              
-              for (cmpd in seq_len(length(unique_cd_compounds_ids))){
-                  sub_df <- CD_data[unique_cd_compounds_ids[cmpd]:unique_cd_compounds_ids_end[cmpd],]
-                  sub_df$Checked <- NULL
-                  
-                  sub_df <- sub_df[-c(1), ]
-                  
-                  sub_colnames <- sub_df[1,]
-                  names(sub_colnames) <- NULL
-                  
-                  sub_df <- sub_df[-c(1), ]
-                  #colnames(sub_df) <- sub_colnames
-                  sub_df$Name <- NULL
-                  
-                  file_hits <- which(sub_df$Formula!="Checked" & sub_df$Formula!="FALSE")
-                  if (length(file_hits)!=1){
-                      file_hits_end <- c((file_hits-1)[2:length(file_hits)], nrow(sub_df))
-                  } else {
-                      file_hits_end <- nrow(sub_df)
-                  }
-                  
-                  features_list <- vector ("list", length(file_hits))
-                  
-                  for (ff in 1:length(file_hits)){
-                      file_df <- sub_df[file_hits[ff]:file_hits_end[ff], ]
-                      file_df_colnames <- file_df[2, ]
-                      names(file_df_colnames) <- NULL
-                      
-                      no_na_names <- which(!is.na(file_df_colnames))
-                      
-                      file_df <- file_df[-c(1,2),no_na_names]
-                      colnames(file_df) <- file_df_colnames[no_na_names]
-                      
-                      features_list[[ff]] <- data.frame(Compound=cd_compounds[cmpd],
-                                                        Formula=cd_formula[cmpd], RT=as.numeric(file_df$`RT [min]`)*60, #RT = cd_RT[cmpd],
-                                                        mzcloud_score=cd_mzcloud_score[cmpd], # gets replaced
-                                                        Ion=file_df$Ion, Charge=file_df$Charge, 
-                                                        mz=as.numeric(file_df$`m/z`),
-                                                        area=as.numeric(file_df$Area))
-                  }
-                  
-                  features_list <- do.call(rbind, features_list)
-                  features_list <- as.data.frame(features_list %>% dplyr::group_by(.data$Ion) %>%
-                                                     dplyr::summarise(Compound      = unique(.data$Compound),
-                                                                      Formula       = unique(.data$Formula),
-                                                                      RT            = mean(.data$RT),
-                                                                      mzcloud_score = mean(.data$mzcloud_score),
-                                                                      Charge        = unique(.data$Charge),
-                                                                      mz            = mean(.data$mz),
-                                                                      area          = max(.data$area),
-                                                                      file_count    = n()))
-                  
-                  # flag most common adduct across input files
-                  hi = max(features_list$file_count)
-                  w=which(features_list$file_count==hi)
-                  file_count_n=features_list[w,]
-                  # if multiple with same count, choose largest area
-                  v = which.max(file_count_n$area)
-                  
-                  features_list$ref_ion = 0
-                  features_list$ref_ion[w[v]] = 1
-                  
-                  cd_compounds_table[[cmpd]] <- features_list
-              }
-              
-              #cd_compounds_table <- do.call(rbind, cd_compounds_table)
-              
-              #Similarly parse table of compounds with similar hits as the highest
-              unique_cd_compounds_ids <-which(!is.na(CD_isomers$Checked))
-              
-              if (length(unique_cd_compounds_ids)>1){
-                  unique_cd_compounds_ids_end <-
-                      c((unique_cd_compounds_ids-1)[2:length(unique_cd_compounds_ids)],
-                        nrow(CD_isomers))
-              } else {
-                  unique_cd_compounds_ids_end <- nrow(CD_isomers)
-              }
-              
-              cd_isomers_table <- vector("list", length(unique_cd_compounds_ids))
-              
-              for (cmpd in seq_len(length(unique_cd_compounds_ids))){
-                  sub_df <- CD_isomers[unique_cd_compounds_ids[cmpd]:unique_cd_compounds_ids_end[cmpd],]
-                  sub_df$Checked <- NULL
-
-                  sub_df <- sub_df[-c(1), ]
-                  
-                  sub_df$Name <- NULL
-                  
-                  sub_colnames <- unlist(sub_df[1,])
-                  names(sub_colnames) <- NULL
-                  
-                  sub_df <- sub_df[-c(1), ]
-                  #colnames(sub_df) <- sub_colnames
-                    
-                  name_col=which(sub_colnames=='Name')
-                  score_col=which(sub_colnames=='Match')
-                  kegg_col=which(sub_colnames=='KEGG ID')
-                  cpd_match=which(sub_colnames=='Compound Match')
-                  ppm_diff=which(sub_colnames=='DeltaMass [ppm]')
-                  
-                  #cmpd_names <- paste (sub_df[[name_col]], collapse=" || ")
-                  #cmpd_cd_score <- paste (round(as.numeric(sub_df[[score_col]]), 2), collapse=" || ")
-                  #cpmd_kegg_id <- paste (sub_df[[kegg_col]], collapse=" || ")
-
-
-                  
-                  cd_isomers_table[[cmpd]] <- data.frame(Compound=sub_df[[name_col]], 
-                                                         mzcloud_score=sub_df[[score_col]],
-                                                         kegg_id = sub_df[[kegg_col]],
-                                                         compound_match=sub_df[[cpd_match]],
-                                                         ppm_diff = sub_df[[ppm_diff]])
-              }
-              
-              # Replace compound names and score in cd_compound_table with all matching names and CD scores
-              for (cmpd in seq_len(length(cd_compounds_table))){
-                  # duplicate records
-                  record=cd_compounds_table[[cmpd]]
-                  record_list=rep(list(record),nrow(cd_isomers_table[[cmpd]]))
-                  record_list=do.call(rbind,record_list)
-                  # update with isomers
-                  record_list=record_list[order(record_list$Ion),]
-                  record_list$Compound = cd_isomers_table[[cmpd]]$Compound
-                  record_list$mzcloud_score = cd_isomers_table[[cmpd]]$mzcloud_score
-                  record_list$kegg_id = cd_isomers_table[[cmpd]]$kegg_id
-                  record_list$compound_match = cd_isomers_table[[cmpd]]$compound_match
-                  record_list$compound_no = cmpd
-                  cd_compounds_table[[cmpd]]=record_list
-              }
-              
-              cd_compounds_table <- do.call(rbind, cd_compounds_table)
-              
-              cd_compounds_table[cd_compounds_table=='NA']=NA
-              
-              # add id column
-              cd_compounds_table$id=as.character(1:nrow(cd_compounds_table))
-             
-              # add extra columns if requested
-              if (length(M$add_cols)>0){
-                  for (g in 1:length(M$add_cols)) {
-                      cd_compounds_table[[names(M$add_cols)[g]]]=M$add_cols[[g]]
-                  }
-              }
-
-              # update objects              
-              D$annotations=as.data.frame(cd_compounds_table)
-              D$annotations$mzcloud_score=as.numeric(D$annotations$mzcloud_score)
-              D$annotations$mz=as.numeric(D$annotations$mz)
-              D$annotations$RT=as.numeric(D$annotations$RT)
-              D$annotations$Charge=as.numeric(D$annotations$Charge)
-              D$mz_column = 'mz'
-              D$rt_column = 'RT'
-              D$id_column = 'id'
-              D$tag = M$tag
-              
-              M$imported=D
+    signature = c("cd_source",'lcms_table'),
+    definition = function(M,D) {
         
-              return(M)
-              
-          })
+        CD_data <- openxlsx::readWorkbook(xlsxFile=M$input_file, sheet=2)
+        CD_isomers <- openxlsx::readWorkbook(xlsxFile=M$compounds_file, sheet=2)
+        
+        if (M$cd_version==3.3) {
+            # replace all 'Tags' with NA
+            CD_data[CD_data=='Tags']=NA
+            CD_isomers[CD_isomers=='Tags']=NA
+            # remove Tags column
+            CD_data$Tags = NULL
+            CD_isomers$Tags = NULL
+        }
+        unique_cd_compounds_ids <-which(!is.na(CD_data$Checked))
+        cd_compounds <- CD_data$Name[unique_cd_compounds_ids]
+        cd_formula <- gsub(" ", "", CD_data$Formula[unique_cd_compounds_ids])
+        cd_RT <- as.numeric(CD_data$`RT.[min]`[unique_cd_compounds_ids]) * 60
+        cd_mzcloud_score <- as.numeric(CD_data$mzCloud.Best.Match[unique_cd_compounds_ids])
+        cd_mz <- as.numeric(CD_data$`m/z`[unique_cd_compounds_ids])
+        
+        if (length(unique_cd_compounds_ids)>1){
+            unique_cd_compounds_ids_end <- c((unique_cd_compounds_ids-1)[2:length(unique_cd_compounds_ids)], nrow(CD_data))
+        } else {
+            unique_cd_compounds_ids_end <- nrow(CD_data)
+        }
+        
+        # CD output is stored in weird tree structure, compounds -> compounds_per_files -> features
+        # this loop will create summary feature table
+        cd_compounds_table <- vector("list", length(cd_compounds))
+        
+        for (cmpd in seq_len(length(unique_cd_compounds_ids))){
+            sub_df <- CD_data[unique_cd_compounds_ids[cmpd]:unique_cd_compounds_ids_end[cmpd],]
+            sub_df$Checked <- NULL
+            
+            sub_df <- sub_df[-c(1), ]
+            
+            sub_colnames <- sub_df[1,]
+            names(sub_colnames) <- NULL
+            
+            sub_df <- sub_df[-c(1), ]
+            #colnames(sub_df) <- sub_colnames
+            sub_df$Name <- NULL
+            
+            file_hits <- which(sub_df$Formula!="Checked" & sub_df$Formula!="FALSE")
+            if (length(file_hits)!=1){
+                file_hits_end <- c((file_hits-1)[2:length(file_hits)], nrow(sub_df))
+            } else {
+                file_hits_end <- nrow(sub_df)
+            }
+            
+            features_list <- vector ("list", length(file_hits))
+            
+            for (ff in 1:length(file_hits)){
+                file_df <- sub_df[file_hits[ff]:file_hits_end[ff], ]
+                file_df_colnames <- file_df[2, ]
+                names(file_df_colnames) <- NULL
+                
+                no_na_names <- which(!is.na(file_df_colnames))
+                
+                file_df <- file_df[-c(1,2),no_na_names]
+                colnames(file_df) <- file_df_colnames[no_na_names]
+                
+                features_list[[ff]] <- data.frame(Compound=cd_compounds[cmpd],
+                    Formula=cd_formula[cmpd], RT=as.numeric(file_df$`RT [min]`)*60, #RT = cd_RT[cmpd],
+                    mzcloud_score=cd_mzcloud_score[cmpd], # gets replaced
+                    Ion=file_df$Ion, Charge=file_df$Charge, 
+                    mz=as.numeric(file_df$`m/z`),
+                    area=as.numeric(file_df$Area))
+            }
+            
+            features_list <- do.call(rbind, features_list)
+            features_list <- as.data.frame(features_list %>% dplyr::group_by(.data$Ion) %>%
+                    dplyr::summarise(Compound      = unique(.data$Compound),
+                        Formula       = unique(.data$Formula),
+                        RT            = mean(.data$RT),
+                        mzcloud_score = mean(.data$mzcloud_score),
+                        Charge        = unique(.data$Charge),
+                        mz            = mean(.data$mz),
+                        area          = max(.data$area),
+                        file_count    = n()))
+            
+            # flag most common adduct across input files
+            hi = max(features_list$file_count)
+            w=which(features_list$file_count==hi)
+            file_count_n=features_list[w,]
+            # if multiple with same count, choose largest area
+            v = which.max(file_count_n$area)
+            
+            features_list$ref_ion = 0
+            features_list$ref_ion[w[v]] = 1
+            
+            cd_compounds_table[[cmpd]] <- features_list
+        }
+        
+        #cd_compounds_table <- do.call(rbind, cd_compounds_table)
+        
+        #Similarly parse table of compounds with similar hits as the highest
+        unique_cd_compounds_ids <-which(!is.na(CD_isomers$Checked))
+        
+        if (length(unique_cd_compounds_ids)>1){
+            unique_cd_compounds_ids_end <-
+                c((unique_cd_compounds_ids-1)[2:length(unique_cd_compounds_ids)],
+                    nrow(CD_isomers))
+        } else {
+            unique_cd_compounds_ids_end <- nrow(CD_isomers)
+        }
+        
+        cd_isomers_table <- vector("list", length(unique_cd_compounds_ids))
+        
+        for (cmpd in seq_len(length(unique_cd_compounds_ids))){
+            sub_df <- CD_isomers[unique_cd_compounds_ids[cmpd]:unique_cd_compounds_ids_end[cmpd],]
+            sub_df$Checked <- NULL
+            
+            sub_df <- sub_df[-c(1), ]
+            
+            sub_df$Name <- NULL
+            
+            sub_colnames <- unlist(sub_df[1,])
+            names(sub_colnames) <- NULL
+            
+            sub_df <- sub_df[-c(1), ]
+            #colnames(sub_df) <- sub_colnames
+            
+            name_col=which(sub_colnames=='Name')
+            score_col=which(sub_colnames=='Match')
+            kegg_col=which(sub_colnames=='KEGG ID')
+            
+            if (as.numeric(M$cd_version)>3.1) {
+                ppm_diff=which(sub_colnames=='DeltaMass [ppm]')
+                cpd_match=which(sub_colnames=='Compound Match')
+            }
+            
+            #cmpd_names <- paste (sub_df[[name_col]], collapse=" || ")
+            #cmpd_cd_score <- paste (round(as.numeric(sub_df[[score_col]]), 2), collapse=" || ")
+            #cpmd_kegg_id <- paste (sub_df[[kegg_col]], collapse=" || ")
+            
+            
+            
+            cd_isomers_table[[cmpd]] <- data.frame(
+                Compound=sub_df[[name_col]], 
+                mzcloud_score=sub_df[[score_col]],
+                kegg_id = sub_df[[kegg_col]])
+            if (as.numeric(M$cd_version)>3.3) {
+                cd_isomers_table$ppm_diff=sub_df[[ppm_diff]]
+                cd_isomers_table$compound_match=sub_df[[cpd_match]]
+            }
+        }
+        
+        
+        # Replace compound names and score in cd_compound_table with all matching names and CD scores
+        for (cmpd in seq_len(length(cd_compounds_table))){
+            # duplicate records
+            record=cd_compounds_table[[cmpd]]
+            record_list=rep(list(record),nrow(cd_isomers_table[[cmpd]]))
+            record_list=do.call(rbind,record_list)
+            # update with isomers
+            record_list=record_list[order(record_list$Ion),]
+            record_list$Compound = cd_isomers_table[[cmpd]]$Compound
+            record_list$mzcloud_score = cd_isomers_table[[cmpd]]$mzcloud_score
+            record_list$kegg_id = cd_isomers_table[[cmpd]]$kegg_id
+            record_list$compound_match = cd_isomers_table[[cmpd]]$compound_match
+            record_list$compound_no = cmpd
+            cd_compounds_table[[cmpd]]=record_list
+        }
+        
+        cd_compounds_table <- do.call(rbind, cd_compounds_table)
+        
+        cd_compounds_table[cd_compounds_table=='NA']=NA
+        
+        # add id column
+        cd_compounds_table$id=as.character(1:nrow(cd_compounds_table))
+        
+        # add extra columns if requested
+        if (length(M$add_cols)>0){
+            for (g in 1:length(M$add_cols)) {
+                cd_compounds_table[[names(M$add_cols)[g]]]=M$add_cols[[g]]
+            }
+        }
+        
+        # update objects              
+        D$annotations=as.data.frame(cd_compounds_table)
+        D$annotations$mzcloud_score=as.numeric(D$annotations$mzcloud_score)
+        D$annotations$mz=as.numeric(D$annotations$mz)
+        D$annotations$RT=as.numeric(D$annotations$RT)
+        D$annotations$Charge=as.numeric(D$annotations$Charge)
+        D$mz_column = 'mz'
+        D$rt_column = 'RT'
+        D$id_column = 'id'
+        D$tag = M$tag
+        
+        M$imported=D
+        
+        return(M)
+        
+    })
 
 
